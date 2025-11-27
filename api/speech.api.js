edcdc21b428534805958043d5f0a260e11544f45
Emiliahrabeson <emiliahrabeson@gmail.com>
2025-11-28 00:52:57 +0300

    Refactor updateMessage function to streamline error handling and remove unnecessary logging

--- api/speech.api.js
+++ api/speech.api.js
@@ -1,17 +1,10 @@
 export const updateMessage = (id) =>
   fetch(`https://bbe-my-eyes.onrender.com/api/v1/speech/${id}`, {
     method: "PATCH",
-  })
-    .then((r) => {
-      if (r.ok) {
-        console.log("Message read");
-        return r.json();
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      console.error(
-        "There was a problem with the fetch operation:",
-        error.message
-      );
-    });
+  }).catch((error) => {
+    console.error(error);
+    console.error(
+      "There was a problem with the fetch operation:",
+      error.message
+    );
+  });
